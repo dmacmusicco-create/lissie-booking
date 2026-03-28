5061ef5d3fef0abda0fc3743ee7360d4e55ca078
Derrick Mackey <dmac.music.co@gmail.com>
2026-03-28 13:59:36 -0500

    Fix trust proxy and email logging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,6 +22,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-secret'],
   credentials: true,
 }));
+app.set('trust proxy', 1);
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
